3554dc7750d226db3a6d31d2b9eec46fdfb668f7
satyasantika <satyasantika@unsil.ac.id>
2025-11-24 09:35:23 +0700

    tambahan icon pada menu admin dashboard

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -9,12 +9,12 @@
             <div class="card-body">
                 @can('read users')
                 Manajemen data user:<br>
-                <a href="{{ route('users.index') }}" class="btn btn-sm btn-primary">User</a>
+                <a href="{{ route('users.index') }}" class="btn btn-sm btn-primary"><i class="bi bi-person"></i> User</a>
                     @can('read roles')
-                    <a href="{{ route('roles.index') }}" class="btn btn-sm btn-primary">Role</a>
+                    <a href="{{ route('roles.index') }}" class="btn btn-sm btn-primary"><i class="bi bi-people"></i> Role</a>
                     @endcan
                     @can('read permissions')
-                    <a href="{{ route('permissions.index') }}" class="btn btn-sm btn-primary">Permission</a>
+                    <a href="{{ route('permissions.index') }}" class="btn btn-sm btn-primary"><i class="bi bi-shield-lock"></i> Permission</a>
                     @endcan
                 <br>
                 <hr>
@@ -22,7 +22,7 @@
 
                 @can('read prodis')
                 manajemen data prodi:<br>
-                <a href="{{ route('prodis.index') }}" class="btn btn-sm btn-primary">Prodi</a>
+                <a href="{{ route('prodis.index') }}" class="btn btn-sm btn-primary"><i class="bi bi-journal-bookmark"></i> Prodi</a>
                 <br>
                 <hr>
                 @endcan
