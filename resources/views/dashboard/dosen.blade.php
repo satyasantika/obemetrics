d7190481f491a05b63ca8ea39ad0e464e6552da6
satyasantika <satyasantika@unsil.ac.id>
2026-01-30 08:32:16 +0700

    perbaikan daftar mata kuliah pada ruang dosen

--- resources/views/dashboard/dosen.blade.php
+++ resources/views/dashboard/dosen.blade.php
@@ -1,154 +1,64 @@
 @push('title')
-    Dashboard Penguji
+    Dashboard Dosen
 @endpush
-<h3>Selamat datang, {{ auth()->user()->name }}</h3>
-
-<div class="row mb-3">
-@can('respon examination score')
-    <div class="col-md-6 mb-3">
+<div class="row mt-3">
+    <div class="col">
         <div class="card">
-            <div class="card-header">Penilaian Ujian</div>
+            <div class="card-header">Manajemen Mata Kuliah oleh Dosen</div>
             <div class="card-body">
-                untuk menilai ujian, silakan klik tombol berikut:<br>
-                <a href="{{ route('scoring.index') }}" class="btn btn-sm btn-primary">Menilai Ujian</a>
-            </div>
-        </div>
-    </div>
-@endcan
-    <div class="col-md-6 mb-3">
-        <div class="card">
-            <div class="card-header">Status Pembimbing dan Penguji</div>
-            <div class="card-body">
-                <a href="{{ route('information.guide') }}" class="btn btn-sm btn-primary mb-2">Daftar</a> Bimbingan saya<br>
-                <a href="{{ route('information.pass') }}" class="btn btn-sm btn-primary mb-2">Bukti</a> Membimbing / Menguji (untuk BKD)
-            </div>
-        </div>
-    </div>
-</div>
+                @include('layouts.alert')
+                {{-- Program Studi --}}
+                @php
+                    $user_id = auth()->id();
+                    $mk_ids = \App\Models\JoinMkUser::where('user_id',$user_id)->pluck('mk_id');
+                    $kurikulum_ids = \App\Models\JoinMkUser::where('user_id',$user_id)->pluck('kurikulum_id')->unique();
+                    $prodi_ids = \App\Models\Kurikulum::whereIn('id',$kurikulum_ids)->pluck('prodi_id');
+                    $prodis = \App\Models\Prodi::whereIn('id',$prodi_ids)->get();
+                @endphp
 
+                @forelse ($prodis as $prodi)
+                    <h4>Program Studi {{ $prodi->jenjang }} {{ $prodi->nama }}</h4>
+                    <hr>
 
-@can('respon selection guide')
-<hr>
-<div class="row">
-    <div class="col-6">
-        <div class="card">
-            <div class="card-header">Info hasil pemilihan pembimbing di tahun 2023</div>
-            <div class="card-body">
-                untuk melihat hasil pemilihan pembimbing tahap 1,2,3,dst... silakan klik tombol berikut:<br>
-                <a href="{{ route('respons.result') }}" class="btn btn-sm btn-primary">Hasil Pemilihan</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-6">
-        <div class="card">
-            <div class="card-header">Terima/Tolak Usulan Pembimbing tahun 2023</div>
-            <div class="card-body">
-                untuk merespon pemilihan pembimbing di tahap 2 silakan klik tommbol berikut:<br>
-                <a href="{{ route('respons.index') }}" class="btn btn-sm btn-primary">proses tahap 2</a>
-            </div>
-        </div>
-    </div>
-</div>
-
-<hr>
-<div class="h5">
-    Informasi kuota Pembimbing
-</div>
-@php
-    $guide = App\Models\GuideAllocation::where('user_id',auth()->user()->id)->where('active',1)->first()
-@endphp
-
-<div class="card">
-    <div class="card-header bg-light">Pembimbing 1</div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col">
-                <div class="h1">
-                    {{ $guide->guide1_quota }}
-                </div>Kuota
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionGuide::where([
-                        'user_id'=>auth()->user()->id,
-                        'guide_order'=>1,
-                        ])->count() }}
-                </div>Usulan
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionStage::where([
-                        'guide1_id'=>auth()->user()->id,
-                        ])->count() }}
-                </div>Acc
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionGuide::where([
-                        'user_id'=>auth()->user()->id,
-                        'guide_order'=>1,
-                        'approved'=>0,
-                        ])->count() }}
-                </div>Ditolak
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionGuide::where([
-                        'user_id'=>auth()->user()->id,
-                        'guide_order'=>1,
-                        'approved'=>NULL,
-                        ])->count() }}
-                </div>Menunggu
+                    {{-- Mata Kuliah --}}
+                    Mata Kuliah pada program studi ini:
+                    @php
+                        $kurikulums = \App\Models\Kurikulum::whereIn('id',$kurikulum_ids)->get();
+                    @endphp
+                    @foreach ($kurikulums as $kurikulum)
+                        @if ($kurikulum->prodi_id == $prodi->id)
+                        @php
+                            $join_mk_users = \App\Models\JoinMkUser::where('kurikulum_id',$kurikulum->id)->pluck('mk_id');
+                            $mks = \App\Models\Mk::whereIn('id',$join_mk_users)->get();
+                        @endphp
+                        <div class="row">
+                            <div class="col">
+                            {{ $kurikulum->nama }}
+                            </div>
+                        </div>
+                        <div class="row mb-1">
+                            <div class="col">
+                                <ol>
+                                @foreach ($mks as $mk)
+                                    <hr>
+                                    <li>
+                                        {{ $mk->kodemk }} {{ $mk->nama }}
+                                        <br>
+                                        <a href="{{ route('mks.cpmks.index',[$mk->id]) }}" class="btn btn-sm btn-primary mt-1">
+                                            <i class="bi bi-sliders"></i> CPMK
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ol>
+                            </div>
+                        </div>
+                        @endif
+                    @endforeach
+                    <hr>
+                @empty
+                    Anda belum terdaftar pada program studi manapun.
+                @endforelse
             </div>
         </div>
     </div>
 </div>
-<div class="card mt-3">
-    <div class="card-header bg-light">Pembimbing 2</div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col">
-                <div class="h1">
-                    {{ $guide->guide2_quota }}
-                </div>Kuota
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionGuide::where([
-                        'user_id'=>auth()->user()->id,
-                        'guide_order'=>2,
-                        ])->count() }}
-                </div>Usulan
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionStage::where([
-                        'guide2_id'=>auth()->user()->id,
-                        ])->count() }}
-                </div>Acc
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionGuide::where([
-                        'user_id'=>auth()->user()->id,
-                        'guide_order'=>2,
-                        'approved'=>0,
-                        ])->count() }}
-                </div>Ditolak
-            </div>
-            <div class="col">
-                <div class="h1">
-                    {{ App\Models\SelectionGuide::where([
-                        'user_id'=>auth()->user()->id,
-                        'guide_order'=>2,
-                        'approved'=>NULL,
-                        ])->count() }}
-
-                </div>Menunggu
-            </div>
-        </div>
-    </div>
-</div>
-@endcan
-
-{{-- @includeWhen(auth()->user()->can('respon selection guide'), 'selection.guide-respon') --}}
--- resources/views/dashboard/prodi.blade.php
+++ resources/views/dashboard/prodi.blade.php
@@ -1,11 +1,10 @@
 @push('title')
     Dashboard Program Studi
 @endpush
-<h3>Selamat datang, {{ auth()->user()->name }}</h3>
 <div class="row">
     <div class="col">
-        <div class="card">
-            <div class="card-header">Manajemen OBE pada oleh UPPS</div>
+        <div class="card bg-primary bg-opacity-10 mt-3">
+            <div class="card-header"><span class="h4">Manajemen Kurikulum OBE oleh UPPS</span></div>
             <div class="card-body">
                 @include('layouts.alert')
                 {{-- Program Studi --}}
@@ -13,53 +12,53 @@
                     $user_id = auth()->id();
                     $prodi_ids = \App\Models\JoinProdiUser::where('user_id',$user_id)->pluck('prodi_id');
                     $prodis = \App\Models\Prodi::whereIn('id',$prodi_ids)->get();
-                    $kurikulums = \App\Models\Kurikulum::whereIn('prodi_id',$prodi_ids)->get();
                 @endphp
 
                 @forelse ($prodis as $prodi)
-                    <h4>Program Studi {{ $prodi->jenjang }} {{ $prodi->nama }}</h4>
+                    <span class="h5">Program Studi {{ $prodi->jenjang }} {{ $prodi->nama }}</span>
                     <hr>
+                    @php
+                        $kurikulums = \App\Models\Kurikulum::where('prodi_id',$prodi->id)->get();
+                    @endphp
 
                     {{-- Kurikulum --}}
                     Kurikulum pada program studi ini:
                     <ol>
                         @forelse ($kurikulums as $kurikulum)
-                            @if ($kurikulum->prodi_id == $prodi->id)
-                            <div class="row mb-1">
-                                <div class="col">
-                                    <li>
-                                        {{ $kurikulum->nama }}
-                                        <a href="{{ route('prodis.kurikulums.edit',[$prodi->id,$kurikulum->id]) }}" class="text-primary">
-                                            <i class="bi bi-pencil-square"></i>
-                                        </a>
-                                        <br>
-                                        <a href="{{ route('kurikulums.profils.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
-                                            <i class="bi bi-mortarboard"></i> Profil Lulusan
-                                        </a>
-                                        <a href="{{ route('kurikulums.cpls.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
-                                            <i class="bi bi-bullseye"></i> CPL
-                                        </a>
-                                        <a href="{{ route('kurikulums.bks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
-                                            <i class="bi bi-book"></i> BK
-                                        </a>
-                                        <a href="{{ route('kurikulums.mks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
-                                            <i class="bi bi-journal-bookmark"></i> MK
-                                        </a>
-                                        <br>
-                                        <a href="{{ route('kurikulums.joinprofilcpls.index',[$kurikulum->id]) }}" class="btn btn-sm btn-secondary mt-1">
-                                            <i class="bi bi-gear"></i> Interaksi Profil >< CPL
-                                        </a>
-                                        <a href="{{ route('kurikulums.joincplbks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-secondary mt-1">
-                                            <i class="bi bi-gear"></i> Interaksi CPL >< BK
-                                        </a>
-                                        <a href="{{ route('kurikulums.joinbkmks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-secondary mt-1">
-                                            <i class="bi bi-gear"></i> Interaksi BK >< Mata Kuliah
-                                        </a>
-                                    </li>
-                                </div>
+                        <hr>
+                        <div class="row mb-1">
+                            <div class="col">
+                                <li>
+                                    {{ $kurikulum->nama }}
+                                    <a href="{{ route('prodis.kurikulums.edit',[$prodi->id,$kurikulum->id]) }}" class="text-primary">
+                                        <i class="bi bi-pencil-square"></i>
+                                    </a>
+                                    <br>
+                                    <a href="{{ route('kurikulums.profils.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
+                                        <i class="bi bi-mortarboard"></i> Profil Lulusan
+                                    </a>
+                                    <a href="{{ route('kurikulums.cpls.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
+                                        <i class="bi bi-bullseye"></i> CPL
+                                    </a>
+                                    <a href="{{ route('kurikulums.bks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
+                                        <i class="bi bi-book"></i> BK
+                                    </a>
+                                    <a href="{{ route('kurikulums.mks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-primary mt-1">
+                                        <i class="bi bi-journal-bookmark"></i> MK
+                                    </a>
+                                    <br>
+                                    <a href="{{ route('kurikulums.joinprofilcpls.index',[$kurikulum->id]) }}" class="btn btn-sm btn-secondary mt-1">
+                                        <i class="bi bi-gear"></i> Interaksi Profil >< CPL
+                                    </a>
+                                    <a href="{{ route('kurikulums.joincplbks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-secondary mt-1">
+                                        <i class="bi bi-gear"></i> Interaksi CPL >< BK
+                                    </a>
+                                    <a href="{{ route('kurikulums.joinbkmks.index',[$kurikulum->id]) }}" class="btn btn-sm btn-secondary mt-1">
+                                        <i class="bi bi-gear"></i> Interaksi BK >< Mata Kuliah
+                                    </a>
+                                </li>
                             </div>
-                            <hr>
-                            @endif
+                        </div>
                         @empty
                             Tidak ada kurikulum pada program studi ini.
                         @endforelse
