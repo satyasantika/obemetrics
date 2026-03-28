c9ca9ce1665d02043888b17adeb9d69b537959b2
satyasantika <satyasantika@unsil.ac.id>
2026-03-29 06:07:22 +0700

    fitur ganti password disesuaikan dengan panel

--- resources/views/auth/passwords/change.blade.php
+++ resources/views/auth/passwords/change.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.panel')
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Change Password') }}</div>
